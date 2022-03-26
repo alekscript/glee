9fc1713e7705d055157d8a0512b4850377572417
alekscript <piatopal.alex@gmail.com>
2022-03-26 02:01:31 +0200

    added section slider. upgrade icons to svg

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
    
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+
 });
